faec6eb6d1e6ec5cd0d50495921b8cd1e1319f9a
xing <499884734@qq.com>
2025-04-16 15:17:17 +0800

    rm some pack

--- build/lib/simpleui_ext/__init__.py
+++
@@ -1,9 +0,0 @@
-"""
-simpleui_ext - Element UI风格的Django admin扩展
-"""
-
-default_app_config = "simpleui_ext.apps.SimpleuiExtConfig"
-
-
-def get_version():
-    return "2025.04.03"
--- build/lib/simpleui_ext/apps.py
+++
@@ -1,7 +0,0 @@
-from django.apps import AppConfig
-
-
-class SimpleuiExtConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'simpleui_ext'
-    verbose_name = 'simpleui extension'
